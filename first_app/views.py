e8c9773b1a7ac9990b8f0e6c285da9d474633be1
Zahra Mughais <zahramughais@gmail.com>
2022-06-11 15:14:20 +0300

    first project django

---
+++ first_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.http import JsonResponse
+
+def root(request):
+    return redirect("/blogs")
+
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect("/")
+
+def show(request, number):
+    return HttpResponse(f"placeholder to display blog number {number}")
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog {number}")
+
+def destroy(request, number):
+    return redirect("/blogs")
+
+def json_res(request):
+    return JsonResponse({"title": "JSON response from blogs", "content keys": ["blogs", "blogs/new", "blogs/create", "blogs/num",
+    "blogs/num/edit", "blogs/num/delete", "blogs/jason"]})
